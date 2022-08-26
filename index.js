727724dbb1cd1e0ae58513c03d93e6598aa9445d
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-08-26 21:40:27 +0600

    First commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello From Minimalo Blog')
+  res.send('Hello From Minimalo-Blog')
 })
 
 app.listen(port, () => {
